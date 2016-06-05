ec4719d6767cc254c4940aad736da0b2a5752da1
Viscoze <vlad.oganov@gmail.com>
2016-06-05 13:56:58 +0300

    Adjust Subjects controller; add Labs controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -3,16 +3,35 @@ class SubjectsController < ApplicationController
     @subjects = Subject.all.order('id ASC')
   end
 
+  def show
+    @subject =  Subject.find(params[:id])
+  end
+
   def new
     @subject = Subject.new
   end
 
   def create
     @subject  = Subject.new subject_params
+
     if @subject.save
       redirect_to root_url
     else
       render :new
+    end
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+
+    if @subject.update(subject_params)
+      redirect_to root_url
+    else
+      render :edit
     end
   end
 
@@ -25,7 +44,7 @@ class SubjectsController < ApplicationController
   private
 
   def subject_params
-    params.require(:subject).permit(:title, :description, :quantity_of_labs:,
+    params.require(:subject).permit(:title, :description, :quantity_of_labs,
                                     :deadline, :priority)
   end
 end
